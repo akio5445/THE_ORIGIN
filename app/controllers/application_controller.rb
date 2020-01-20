72f1740dd741600cf06ea15223c8b54c15155cdf
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-01-20 20:09:03 +0900

    403エラー画面を変更した
    * view/errors/forbidden_error.html.erbを追加しエラー時の表示を変更
     - @exceptionで引数に渡された（e）例外オブジェクトの種類によって表示を切り替えるようcase文を追加
    * application_controller.rbに例外を補足する記述を追加
     - +3class Forbiddenを定義
     - +4class IpAddressRejectedを定義
     - +7rescue_formで上記の例外発生時rescue403に処理が移行する
     - +8rescue_formで上記の例外発生時rescue403に処理が移行する
     - +11~privateとしてrescueの処理を追加 上記追加したviewにrenderするよう設定 status: 403 にてHTTPレスポンスのステータスコードを403に設定
     - 継承関係、エラーステータス説明コメント#を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
-  rescue_from StandardError, with: :rescue500
 
-  private def rescue500
-    render "errors/internal_server_error", status: 500
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  rescue_from StandardError, with: :rescue500
+  rescue_from ApplicationController::Forbidden, with: :rescue403
+  rescue_from ApplicationController::IpAddressRejected, with: :rescue403
+
+
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
+    # 403は要求されらリソースがwebサイトに存在するがなんらかの理由でアクセス拒否された時
   end
+
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
+    #　500は何らかのエラーが発生してる時
+  end
+  # ActionController::ActionControllerError < StandardError < Exception
 end
